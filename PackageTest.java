9bddd4db07f7c00752f392a1f847da2c8088a0eb
Sha Jan-wee <brunoxia@outlook.com>
2018-08-24 21:22:42 +0800

    25
    25.包
    a)	Java中要想使用包，必须先声明，格式如下:package [包名];
    b)	Java虚拟机默认包java.lang的使用不需要声明。

---
+++ PackageTest.java
@@ -0,0 +1,13 @@
+package package_2;
+import java.util.Date;
+
+public class PackageTest {
+	public static void main(String[] args)
+	{
+		java.util.Date today=new java.util.Date();
+		Date present = new Date();
+		System.out.println(today);
+		System.out.println(present);
+	}
+	
+}
