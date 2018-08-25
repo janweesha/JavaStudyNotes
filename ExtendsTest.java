c45ee0739e06e6c103d757178e9071d9815c7740
Sha Jan-wee <brunoxia@outlook.com>
2018-08-25 20:07:02 +0800

    32
    32.类的继承

---
+++ ExtendsTest.java
@@ -0,0 +1,85 @@
+public class ExtendsTest {
+	
+	public static void main(String[] args)
+	{
+		Student4 tom=new Student4("Janwee","001","Man");
+		tom.setName("Janwee");
+		tom.setGender("Man");
+		tom.setId("001");
+		System.out.println(tom.toString());
+		
+		Manager jerry=new Manager("Jerry","002","Man","Monitor");
+		System.out.println(jerry.toString());
+	}
+}
+
+class Student4
+	{
+		private String name="";
+		private String id="";
+		private String gender="";
+		public Student4(String n,String i,String g)//构造函数(与类同名)
+		{
+			id=i;
+			name=n;
+			gender=g;
+		}
+		public String getName()//获取名字
+		{
+			return name;
+		}
+		public void setName(String n)//设置姓名
+		{
+			name=n;
+		}
+		public String getId()//获取ID
+		{
+			return id;
+		}
+		public String getGender()//获取性别
+		{
+			return gender;
+		}
+		public void setGender(String g)//设置性别
+		{
+			gender=g;
+		}
+		public void setId(String i)//设置id
+		{
+			id=i;
+		}
+		public String toString()
+		{
+			String info="Name:"+name+"\tId:"+id+"\tGender:"+gender;
+			return info;
+		}
+			
+	}
+
+class Manager extends Student4
+{
+	private String job="";
+	
+	public String getJob()
+	{
+		return job;
+	}
+	
+	public void setJob(String j)
+	{
+		job=j;
+	}
+	
+	public String toString()
+	{
+		String str=super.toString();
+		str+="\tJob:"+job;
+		return str;
+	}
+	public Manager(String n,String i,String g,String j)//构造函数(与类同名)
+	{
+		super(n,i,g);
+		job=j;
+	}
+	
+}
